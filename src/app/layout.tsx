ff0b1e3e484460fa55649aa1754573641f2221a7
dsonbaker <araujoedson790@gmail.com>
2026-01-28 02:12:59 -0300

    add: clarity and GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,33 @@ export default async function RootLayout({
             `
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "v8bwhun2br");
+            `
+          }}
+        />
+        {/* Google Analytics */}
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-R5CHTD6X59'
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R5CHTD6X59');
+            `
+          }}
+        />
       </head>
       <body
         className={cn(
